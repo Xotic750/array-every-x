4e08c85ea6620512086a8fee8e67fe8f3f706453
Graham Fairweather <xotic750@gmail.com>
2019-07-18 01:57:26 +0200

    :bookmark: v3.0.9
    Dependency updates

--- dist/array-every-x.js
+++ dist/array-every-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-17T19:11:04.148Z",
+  "date": "2019-07-17T23:57:20.349Z",
   "describe": "",
   "description": "Tests that all elements in the array pass the provided function.",
   "file": "array-every-x.js",
   "hash": "b37cbd663b71dc9cd4cf",
   "license": "MIT",
-  "version": "3.0.8"
+  "version": "3.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
