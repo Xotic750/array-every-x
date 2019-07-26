dd9d4c147aeee6f2f204e6da2f06f2cb4e3eaca0
Graham Fairweather <xotic750@gmail.com>
2019-07-26 16:19:12 +0200

    :bookmark: v3.0.17
    Auto build

--- dist/array-every-x.js
+++ dist/array-every-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-25T22:07:16.994Z",
+  "date": "2019-07-26T14:19:04.628Z",
   "describe": "",
   "description": "Tests that all elements in the array pass the provided function.",
   "file": "array-every-x.js",
   "hash": "1b4a3af845a60b49aa30",
   "license": "MIT",
-  "version": "3.0.16"
+  "version": "3.0.17"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
