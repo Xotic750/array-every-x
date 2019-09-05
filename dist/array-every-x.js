c040129a3af740258ba47015b58cd6616cd5f35a
Graham Fairweather <xotic750@gmail.com>
2019-09-05 21:34:05 +0200

    Maintenance

--- dist/array-every-x.js
+++ dist/array-every-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-09-04T23:25:41.828Z",
+  "date": "2019-09-05T19:33:59.176Z",
   "describe": "",
   "description": "Tests that all elements in the array pass the provided function.",
   "file": "array-every-x.js",
